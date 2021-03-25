e22f1b3b22573328d3a2afd97820224b074eb7ca
Drakunal <drakunaldey1@gmail.com>
2021-03-25 20:40:44 +0530

    test js complete

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
 
   await page.goto('https://ams.acimacredit.com/contract_applications/new?manifest_guid=apmf-063b28b6-9d58-4430-8540-eb15f09ebad7&lang=en.leases' ,{"waitUntil" : "networkidle0"});
@@ -71,9 +71,73 @@ const puppeteer = require('puppeteer');
   const stateCode= await page.$('#contract_applications_forms_contract_application_form_applicant_information_attributes_state_code');
   await stateCode.type(state_code);
 
+  var zip_code="12345";
+  const zipCode= await page.$('#contract_applications_forms_contract_application_form_applicant_information_attributes_zip_code');
+  await zipCode.type(zip_code);
+
+  var email_id="test@test.com";
+  const emailId= await page.$('#contract_applications_forms_contract_application_form_applicant_information_attributes_email');
+  await emailId.type(email_id);
+
+  var dob_month="Jan";
+  const dobMonth= await page.$('#contract_applications_forms_contract_application_form_applicant_identity_attributes_date_of_birth_month');
+  await dobMonth.type(dob_month);
+
+  var dob_day="12";
+  const dobDay= await page.$('#contract_applications_forms_contract_application_form_applicant_identity_attributes_date_of_birth_day');
+  await dobDay.type(dob_day);
+
+
+  var dob_year="1990";
+  const dobYear= await page.$('#contract_applications_forms_contract_application_form_applicant_identity_attributes_date_of_birth_year');
+  await dobYear.type(dob_year);
+
 
   
+
+  var id_type="U";
+  const idType= await page.$('#contract_applications_forms_contract_application_form_applicant_identity_attributes_card_type');
+  await idType.type(id_type);
+
+  var id_number="123456";
+  const idNumber= await page.$('#contract_applications_forms_contract_application_form_applicant_identity_attributes_card_number');
+  await idNumber.type(id_number);
+
+  
+
+  var id_state="AL";
+  const idState= await page.$('#contract_applications_forms_contract_application_form_applicant_identity_attributes_card_state_code');
+  await idState.type(id_state);
+  
+
+
+
+  var bank_routing_number="123456";
+  const bankRoutingNumber= await page.$('#contract_applications_forms_contract_application_form_banking_attributes_routing_number');
+  await bankRoutingNumber.type(bank_routing_number);
+
+  var bank_account_number="123456";
+  const bankAccountNumber= await page.$('#contract_applications_forms_contract_application_form_banking_attributes_account_number');
+  await bankAccountNumber.type(bank_account_number);
+
+  var bank_name="Bank name";
+  const bankName= await page.$('#contract_applications_forms_contract_application_form_banking_attributes_bank_name');
+  await bankName.type(bank_name);
+
+  //selection
+  var income_source="military";
+  const incomeSource= await page.$('#contract_applications_forms_contract_application_form_applicant_primary_income_attributes_income_source');
+  await incomeSource.type(income_source);
+  // await page.select("select#first", income_source)
+
+  var payment_method="direct_deposit";
+  const paymentMethod= await page.$('#contract_applications_forms_contract_application_form_applicant_primary_income_attributes_payment_method');
+  await paymentMethod.type(payment_method);
+
+  var monthly_income="10000";
+  const monthlyIncome= await page.$('#contract_applications_forms_contract_application_form_applicant_primary_income_attributes_monthly_net');
+  await monthlyIncome.type(monthly_income);
   
   await page.screenshot({ path: 'example.png' });
-  await browser.close();
+  // await browser.close();
 })();
