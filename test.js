8d677557c75186c3fb255313b0547d208edea2dc
Drakunal <drakunaldey1@gmail.com>
2021-03-25 16:07:02 +0530

    form opening

--- test.js
+++ test.js
@@ -3,7 +3,33 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://ams.acimacredit.com/contract_applications/new?manifest_guid=apmf-fc04494b-ce3f-47fc-9e2f-af5fa5505c5d&lang=en.leases');
+
+  await page.goto('https://ams.acimacredit.com/contract_applications/new?manifest_guid=apmf-063b28b6-9d58-4430-8540-eb15f09ebad7&lang=en.leases' ,{"waitUntil" : "networkidle0"});
+
+  await Promise.all([
+   
+    await  page.click('a.btn.btn-primary'),
+    await   page.waitForNavigation()
+
+     
+
+  ]);
+  await Promise.all([
+   
+    await page.click('a.btn.btn-lg.btn-primary'), //NEXT
+    await   page.waitForSelector('a.btn.btn-lg.btn-primary')
+
+    // await page.click('a.btn.btn-lg.btn-primary');
+    // await   page.waitForNavigation()
+     
+   
+
+  ]);
+
+   
+  await page.click('a.btn.btn-lg.btn-primary');
+  await   page.waitForNavigation();
+  
   await page.screenshot({ path: 'example.png' });
 
   await browser.close();
