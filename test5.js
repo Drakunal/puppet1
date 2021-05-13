27f356f085fdd3fbbd92f0bcb4a90683e24c4510
Drakunal <drakunaldey1@gmail.com>
2021-05-13 18:15:01 +0530

    american first updated

--- test5.js
+++ test5.js
@@ -2,7 +2,28 @@
 require('dotenv').config()
 const puppeteer = require('puppeteer');
 
-(async () =>{
-    
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto('https://sv1.americanfirstfinance.com/application/7827/1', {
+        "waitUntil": "networkidle0"
+    });
+    //await page.click('input[value="google"][type="submit"]');
+   
+    // await page.waitForTimeout(4000);
+    await Promise.all([
+        await page.click('.aff-button'),
+        await   page.waitForNavigation()
+    ]);
+      await Promise.all([
+        await page.click('button[class~="text-natural"]'),
+        // await page.click('.text-natural.card-button.mx-auto.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.v-size--default'),
+        await   page.waitForNavigation()
+    ]);
 
+    await page.screenshot({
+        path: 'american.png'
+    });
 })();
